17b1094617a1289837b55bfd4156cb397686cb3b
michael <michaelngige@gmail.com>
2023-10-05 19:58:54 +0300

    completed assignent

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-// Add your code here
+
+const submitData= {  
+    method:"POST",
+    headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json",
+      },
+    body:JSON.stringify(
+       {
+        userName:"mike",
+        userEmail:"mike12345@gmail.com"
+       } 
+    )
+}
+
+fetch("http://localhost:3004/users", submitData)
+.then(response=>response.json())
+.then(user=>console.log(user))
+.catch(error => {        
+       const errorMessage = document.createElement("p");
+        errorMessage.textContent = error.message;
+       document.body.appendChild(errorMessage);
+})
+
